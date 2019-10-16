c60428da1e659a30669d84fe250d0680e60895e6
sreenandini <mallelasreenandini2@gmail.com>
2019-10-16 13:37:49 +0100

    added delete  functionality

--- HotelApp/client/src/App.vue
+++ HotelApp/client/src/App.vue
@@ -6,6 +6,7 @@
       <br>
       <p>{{booking.name}}</p>
       <p>{{booking.email}}</p>
+      <button v-on:click="handleDelete(booking._id)">Delete Booking</button>
       <hr>
     </div>
 
@@ -13,6 +14,7 @@
 </template>
 
 <script>
+import {eventBus} from './main';
 import BookingsForm from "./components/BookingsForm.vue"
 import BookingService from "./services/BookingService.js"
 export default {
@@ -24,12 +26,24 @@ export default {
   },
   mounted(){
     this.fetchdata();
+    eventBus.$on('guest-added' ,booking => this.bookings.push(booking));
+
+    eventBus.$on('booking-deleted',id => {
+      const index = this.bookings.findIndex(booking => booking._id == id);
+    this.bookings.splice(index,1);
+  })
   },
   methods:{
     fetchdata(){
       BookingService.getBookings()
       .then(bookings => this.bookings = bookings)
+    },
+    handleDelete(id){
+      BookingService.deleteBooking()
+      .then(booking => eventBus.$emit('booking-deleted',id))
+
     }
+
 
   },
   components: {
--- HotelApp/client/src/components/BookingsForm.vue
+++ HotelApp/client/src/components/BookingsForm.vue
@@ -1,11 +1,14 @@
 <template lang="html">
-  <form id="booking-form" v-on:submit= "handleSubmit" method="post">
+  <form id="booking-form" v-on:submit="handleSubmit" method="post">
     <div id = "booking-form">
       <label for="name">Name</label>
-      <input type="text" id="name" v-model="name">
+      <input type="text" id="name"  v-model="name" required/>
 
       <label for="email">Email</label>
-      <input type="text" id="email" v-model="email">
+      <input type="text" id="email"  v-model="email" required/>
+
+      <label for="check_status">Check-Status</label>
+      <input type="checkbox"  id="check_status" v-model="check_status">
 
       <input type="submit" >
     </div>
@@ -14,10 +17,37 @@
 </template>
 
 <script>
+import {eventBus} from '../main';
+import BookingService from "../services/BookingService.js";
 export default {
-  name: "booking-form"
-  // props:[]
-}
+  name: "booking-form",
+  data(){
+    return {
+      name: "",
+      email: "",
+      check_status:false
+    }
+  },
+
+  methods: {
+    handleSubmit(event){
+      console.log(event);
+      console.log(event.target);
+      console.log(event.target.email.value);
+      event.preventDefault();
+      const payload = {
+        name: this.name,
+        email: this.email,
+        check_status: this.check_status
+      }
+      BookingService.postBooking(payload).then(booking =>{eventBus.$emit('guest-added',booking)})
+      event.target.reset();
+
+    }
+
+  }
+};
+
 </script>
 
 <style lang="css" scoped>
